d8a0fead5274372e6ab7da85e34dfaf91a2dc52c
ilya Pashchenko <in4pashchenko@gmail.com>
2023-06-21 00:26:18 +0300

    Ready to use.

--- clean_uvfits.py
+++ clean_uvfits.py
@@ -5,13 +5,12 @@ sys.path.insert(0, '/home/ilya/github/ve/vlbi_errors')
 from spydiff import CLEAN_difmap, clean_difmap
 
 
-
-os.system(f"parallel -k --jobs {n_jobs} python {script_dir}/clean_uvfits.py --beam_fractions \"{beam_fracs}\" --mapsize_clean \"{mapsize} {mapsizes_dict[freq_ghz][1]}\" --save_dir \"{save_dir}\" --path_to_script \"{path_to_script}\"  --nw_beam_size \"{nw_beam_size}\" --use_elliptical \"{use_elliptical}\" --fname ::: {fnames}")
-
 if __name__ == "__main__":
 
     CLI = argparse.ArgumentParser()
     CLI.add_argument("--fname",
+                     type=str)
+    CLI.add_argument("--epoch",
                      type=str)
     # CLI.add_argument("--beam_fractions",  # name on the CLI - drop the `--` for positional/required parameters
     #                  nargs="*",  # 0 or more values expected => creates a list
@@ -24,7 +23,9 @@ if __name__ == "__main__":
                      default=[512, 0.1],  # default if nothing is provided
                      )
     CLI.add_argument("--beam_restore",  # name on the CLI - drop the `--` for positional/required parameters
-                     type=float
+                     nargs="*",
+                     type=float,
+                     default=[0.7, 0.7, 0]
                      # default=[1.353, 1.557, -65.65],  # default if nothing is provided
                      # default=[4.561, 5.734, -51.67],  # default if nothing is provided
                      )
@@ -35,15 +36,17 @@ if __name__ == "__main__":
     args = CLI.parse_args()
 
     uvfits_file = args.fname
+    epoch = args.epoch
     save_dir = args.save_dir
     beam_restore = args.beam_restore
     path_to_script = args.path_to_script
     mapsize_clean = args.mapsize_clean
 
     stokes = ("I", "Q", "U")
-    epoch = None
+    base_name = os.path.split(uvfits_file)[-1]
+    base_name = base_name.split(".")[0]
     for stk in stokes:
-        outfname = "model_cc_{}_{}.fits".format(epoch, stk.lower())
+        outfname = "{}_{}.fits".format(base_name, stk.lower())
         if os.path.exists(os.path.join(save_dir, outfname)):
             os.unlink(os.path.join(save_dir, outfname))
         clean_difmap(fname=uvfits_file, path=save_dir,
